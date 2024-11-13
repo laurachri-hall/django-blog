c1f0a5046be6877ebcae2877b2aad1aab8846a42
Laura Hall <laura@allthehalls.com>
2024-11-13 16:10:10 +0000

    Add: meta ordering and dunder for Comment class

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.body} | comment by {self.author}"
+
