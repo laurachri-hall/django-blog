12fd1c69597046360ed6be5caab573eeb95eb58a
Laura Hall <laura@allthehalls.com>
2024-11-14 15:10:07 +0000

    Fix: order of paths in url patterns

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
+    path('admin/', admin.site.urls),
+    
     
 ]
