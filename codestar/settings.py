eb906aa1f6a32512f30a251cf9ce15b39743f689
Laura Hall <laura@allthehalls.com>
2024-11-21 10:41:55 +0000

    Add: cloudianry and ability for superuser to upload image with post

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,9 +15,6 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-#     Debug = True
-# else:
-#     Debug = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -62,6 +60,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
